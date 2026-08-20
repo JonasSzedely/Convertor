b5af382a44d24d7a7b13b5b718904e50494820a6
Jonas <jszedely@gmail.com>
2026-08-20 18:14:36 +0200

    new architecture structure and design test

--- app/src/main/java/com/example/spaceconvert/ui/screens/CurrencyCalculator.kt
+++ app/src/main/java/com/example/spaceconvert/ui/screens/CurrencyCalculator.kt
@@ -1,12 +1,8 @@
 package com.example.spaceconvert.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun CurrencyCalculator(
